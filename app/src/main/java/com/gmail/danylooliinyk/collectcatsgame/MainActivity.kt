fd92fe7f3673871e167921228d342e37314a24e4
Danylo Oliinyk <danylo.oliinyk@gmail.com>
2022-06-30 15:48:16 +0300

    Renamed circles to cats

--- app/src/main/java/com/gmail/danylooliinyk/collectcatsgame/MainActivity.kt
+++ app/src/main/java/com/gmail/danylooliinyk/collectcatsgame/MainActivity.kt
@@ -1,9 +1,8 @@
-package com.gmail.danylooliinyk.collectcirclesgame
+package com.gmail.danylooliinyk.collectcatsgame
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.gmail.danylooliinyk.collectcatsgame.CollectCatsGame
 import com.gmail.danylooliinyk.collectcatsgame.view.CatViewContainer
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
